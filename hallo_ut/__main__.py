80ec6ba246c56e95407899b584094710b2a67ab5
hexatester <revolusi147id@gmail.com>
2021-03-07 21:37:53 +0700

    Added hallo.status_tiket

--- hallo_ut/__main__.py
+++ hallo_ut/__main__.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 from argparse import ArgumentParser
 from typing import List
@@ -18,9 +19,17 @@ def main():
         help="pertanyaan tentang Universitas Terbuka",
     )
     args = parser.parse_args()
-    hallo = HalloUt()
     pertanyaan: List[str] = getattr(args, "pertanyaan", list())
     if not pertanyaan:
+        return
+    hallo = HalloUt()
+    match = re.match(r"^(W\d{10}-\d{8})$", " ".join(pertanyaan))
+    if match:
+        for group in match.groups():
+            if not group:
+                continue
+            tiket = hallo.status_tiket(group)
+            print(str(tiket))
         return
     results = hallo.faq(" ".join(pertanyaan))
     if results:
