9ffa0cde8f2549642e193508531d5c8ee492f5ff
hexatester <revolusi147id@gmail.com>
2021-03-07 18:42:57 +0700

    Add logging

--- hallo_ut/hallo_ut.py
+++ hallo_ut/hallo_ut.py
@@ -1,4 +1,5 @@
 import requests
+import logging
 from fuzzywuzzy import process
 from typing import List
 
@@ -7,6 +8,7 @@ from . import Faq
 
 class HalloUt:
     def __init__(self):
+        self.logger = logging.getLogger(self.__class__.__name__)
         self.faqs = Faq.fetch_faqs()
         self.faq_q_dict = dict(enumerate([faq.question for faq in self.faqs]))
 
@@ -14,7 +16,9 @@ class HalloUt:
         return self.faq(query, limit)
 
     def faq(self, query: str, limit: int = 10, score_cutoff: int = 60) -> List[Faq]:
+        self.logger.debug(f"Mencari faq dengan keyword `{query}`")
         best_faq = process.extractBests(
             query, self.faq_q_dict, score_cutoff=score_cutoff, limit=10
         )
+        self.logger.debug(f"Ditemukan {len(best_faq)} kemungkinan jawaban")
         return [self.faqs[z] for (x, y, z) in best_faq] if best_faq else []
