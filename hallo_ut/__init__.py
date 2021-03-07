f4382a32a06e67bbe5ab1d698a7d00c2af19e4c7
hexatester <revolusi147id@gmail.com>
2021-03-07 17:50:46 +0700

    Add HalloUt

--- hallo_ut/__init__.py
+++ hallo_ut/__init__.py
@@ -1,8 +1,9 @@
 from .faq import Faq
+from .hallo_ut import HalloUt
 
 
 __author__ = "hexatester <habibrohman@protonmail.com>"
 __version__ = "0.1.0"
 
 
-__all__ = ["Faq"]
+__all__ = ["Faq", "HalloUt"]
---
+++ hallo_ut/hallo_ut.py
@@ -0,0 +1,20 @@
+import requests
+from fuzzywuzzy import process
+from typing import List
+
+from . import Faq
+
+
+class HalloUt:
+    def __init__(self):
+        self.faqs = Faq.fetch_faqs()
+        self.faq_q_dict = dict(enumerate([faq.question for faq in self.faq]))
+
+    def __call__(self, query: str, limit: int = 10) -> List[Faq]:
+        return self.faq(query, limit)
+
+    def faq(self, query: str, limit: int = 10, score_cutoff: int = 60) -> List[Faq]:
+        best_faq = process.extractBests(
+            query, self.faq_q_dict, score_cutoff=score_cutoff, limit=10
+        )
+        return [self.faqs[z] for (x, y, z) in best_faq] if best_faq else []
